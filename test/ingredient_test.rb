632f89f6ae2147e467275cf8de498b1353fae66b
Matt Levy <milevy1@gmail.com>
2019-02-26 09:09:30 -0700

    Added test it exists

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -3,4 +3,12 @@ require './lib/ingredient'
 
 class IngredientTest < Minitest::Test
 
+  def setup
+    @cheese = Ingredient.new("Cheese", "oz", 50)
+  end
+
+  def test_it_exists
+    assert_instance_of Ingredient, @cheese
+  end
+
 end
