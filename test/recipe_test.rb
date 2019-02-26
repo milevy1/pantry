065b04f7b3bfa2c380d23e3389cc329e3e714d8f
Matt Levy <milevy1@gmail.com>
2019-02-26 09:24:04 -0700

    Added test_it_can_query_amount_required_of_ingredient

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -35,4 +35,12 @@ class RecipeTest < Minitest::Test
     assert_equal expected, @mac_and_cheese.ingredients_required
   end
 
+  def test_it_can_query_amount_required_of_ingredient
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal 2, @mac_and_cheese.amount_required(@cheese)
+    assert_equal 8, @mac_and_cheese.amount_required(@mac)
+  end
+
 end
