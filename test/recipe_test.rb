182a57e3563e3d9249da4dfc9f5673196b1f44b4
Matt Levy <milevy1@gmail.com>
2019-02-26 09:28:00 -0700

    Added test_it_can_total_calories_of_all_ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -50,4 +50,11 @@ class RecipeTest < Minitest::Test
     assert_equal [@cheese, @mac], @mac_and_cheese.ingredients
   end
 
+  def test_it_can_total_calories_of_all_ingredients
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal 440, @mac_and_cheese.total_calories
+  end
+
 end
