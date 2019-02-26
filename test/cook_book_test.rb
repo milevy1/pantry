595a536169b332f2f416cb8082bee0ac5194c880
Matt Levy <milevy1@gmail.com>
2019-02-26 09:53:10 -0700

    Added setup

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -6,4 +6,22 @@ require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
 
+  def setup
+    @cookbook = CookBook.new
+
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    @ground_beef = Ingredient.new("Ground Beef", "oz", 100)
+    @bun = Ingredient.new("Bun", "g", 1)
+
+    @burger = Recipe.new("Burger")
+    @burger.add_ingredient(@ground_beef, 4)
+    @burger.add_ingredient(@bun, 100)
+  end
+
 end
